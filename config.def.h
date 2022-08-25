e2a691a5972bbefbacd86aaed312b87e916a2a62
Adam Salhi <adamtherookie@proton.me>
2022-08-25 16:53:30 +0100

    Added config file and install thingy

---
+++ config.def.h
@@ -0,0 +1,11 @@
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+static char *launcher[] = {"dmenu_run", NULL};
+static char *terminal[] = {"xterm", NULL};
+
+static char *border_main = "#1b496e";
+static char *border_sec = "#7a7a7a";
+
+#endif
+
